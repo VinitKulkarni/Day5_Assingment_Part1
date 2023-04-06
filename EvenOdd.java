adccf68fb4c08c3ff01a0b8558f71abf4d8ee9b8
vinit <vinitkkulkarni@gmail.com>
2023-04-06 18:17:05 +0530

    Added:logic for even,odd number

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.Day5P1;
+
+//8. Java Program to Check Whether a Number is Even or Odd
+public class EvenOdd {
+    public static void main(String[] args) {
+        int data = 7;
+
+        if(data%2 == 0){
+            System.out.println(data + " is even number");
+        }else{
+            System.out.println(data + " is odd number");
+        }
+    }
+}
